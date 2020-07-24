d4c710f8462ef9c7072279e8b3ccc0c48ba514b1
manchandajayant <maanjayant@gmail.com>
2020-07-24 11:59:47 +0200

    fav button added with functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { Fragment, useContext, useEffect } from "react";
 import { Store } from "./store";
+import { IAction, IEpisode } from "./interfaces";
 
 function App(): JSX.Element {
   const { state, dispatch } = useContext(Store);
@@ -15,25 +16,52 @@ function App(): JSX.Element {
       payload: dataJSON._embedded.episodes,
     });
   };
+
   useEffect(() => {
     state.episodes.length === 0 && fetchDataAction();
   });
+
+  const toggleFavAction = (episode: IEpisode): IAction => {
+    const episodeInFav = state.favourites.includes(episode);
+    let dispatchObj = {
+      type: "ADD_FAV",
+      payload: episode,
+    };
+    if (episodeInFav) {
+      const favWithoutEpisode = state.favourite.filter(
+        (fav: IEpisode) => fav.id !== episode.id
+      );
+      dispatchObj = {
+        type: "REMOVE_FAV",
+        payload: favWithoutEpisode,
+      };
+    }
+    dispatch(dispatchObj);
+  };
+
   return (
     <Fragment>
-      <h1>Rick and Morty Episode Picker</h1>
-      <p>Pick one</p>
-
-      <section>
-        {state.episodes.map((episode: any) => {
+      <header className="header">
+        <h1>Rick and Morty Episode Picker</h1>
+        <p>Pick one</p>
+      </header>
+      <section className="episode-layout">
+        {state.episodes.map((episode: IEpisode) => {
           return (
-            <section key={episode.id}>
+            <section key={episode.id} className="episode-box">
               <img
                 src={episode.image.medium}
                 alt={`Rick and mort${episode.name}`}
               />
               <div>{episode.name}</div>
               <section>
-                Season:{episode.season}Number:{episode.number}
+                <div>
+                  {" "}
+                  Season:{episode.season}Number:{episode.number}
+                </div>
+                <button type="button" onClick={() => toggleFavAction(episode)}>
+                  Fav
+                </button>
               </section>
             </section>
           );
